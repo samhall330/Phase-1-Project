2d3f4cb50d52a511fd74ca079a756da5afbb6545
Melissa Faulner <melissa.faulner@gmail.com>
2021-01-05 16:02:32 -0500

    setting up tty-prompt

--- Ruby-CLI-Setup/app/yourAppName.rb
+++ Ruby-CLI-Setup/app/yourAppName.rb
@@ -1,13 +1,23 @@
 class YourAppName
   # here will be your CLI!
   # it is not an AR class so you need to add attr
-
+  attr_reader :prompt
+  
+  def initialize
+    @prompt = TTY::Prompt.new
+  end
+  
   def run
     # welcome
+    def welcome
+      
+    end
     # login_or_signup
     # wanna_see_favs?
     # get_joke(what_subject)
   end
+
+  
 
   private
 
